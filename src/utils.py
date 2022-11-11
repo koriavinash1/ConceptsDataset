0ed9bc0b198246721bd257fcffb56cdbe6108ac7
koriavinash1 <avin.kori.re@gmail.com>
2022-11-11 10:31:19 +0000

    notebook test

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import cv2
 from imgaug import augmenters as iaa
 from matplotlib.patches import RegularPolygon
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.ticker import FormatStrFormatter
 import numpy as np
 import math
 
@@ -12,20 +15,23 @@ class CreateObject(object):
         self.transform = iaa.Sequential([
                             iaa.Affine(rotate=(-180, 180),
                                         translate_percent={"x": (-0.38, 0.38), "y": (-0.38, 0.38)},
-                                        shear=(-25, 25)),
+                                        shear=(-25, 25),
+                                        mode='symmetric'),
                             iaa.AdditiveGaussianNoise(scale=(10, 60)),
                         ]) 
 
 
         self.objects = {'circle': self.create_circle,
                         'square': self.create_square,
-                        'hexagon': self.create_hex,
+                        'hexagon': self.create_polygon(6),
+                        'pentagon': self.create_polygon(5),
+                        'octagon': self.create_polygon(8),
                         'ellipse': self.create_ellipse,
                         'capsule': self.create_capsule,
-                        'triangle': self.create_ellipse}
+                        'triangle': self.create_triangle}
 
 
-    def create_canvas(self, background-(255, 255, 255)):
+    def create_canvas(self, background=(255, 255, 255)):
         img = np.uint8(np.zeros((self.height, self.width, 3)))
         img[:,:, 0] = background[0]
         img[:,:, 1] = background[1]
@@ -35,16 +41,17 @@ class CreateObject(object):
 
     def create_square(self, background=(255, 255, 255)):
         img = self.create_canvas(background)
-        object_height = np.random.randint(0, self.max_object)
-        object_width = np.random.randint(0, self.max_object)
+        object_height = np.random.randint(10, self.max_object)
+        object_width = np.random.randint(10, self.max_object)
 
-        start_point = (self.width//2 - object_width//2, self.height//2 - object_height//2)
-        end_point = (self.width//2 + object_width//2, self.height//2 + object_height//2)
-        color = tuple(np.random.randint(0, 255, 3))
+        start_point = (int(self.width//2 - object_width//2), int(self.height//2 - object_height//2))
+        end_point = (int(self.width//2 + object_width//2), int(self.height//2 + object_height//2))
+        color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
+
         img = cv2.rectangle(img, 
-                pt1=start_point, 
-                pt2=end_point, 
-                color=color, 
+                start_point, 
+                end_point, 
+                color, 
                 thickness=-1)
         return img
 
@@ -62,41 +69,41 @@ class CreateObject(object):
         
 
         # translate points to center
-        p1 = (p1[0] + self.height//2, p1[1] + self.width//2)
-        p2 = (p2[0] + self.height//2, p2[1] + self.width//2)
-        p3 = (p3[0] + self.height//2, p3[1] + self.width//2)
+        p1 = (int(p1[0] + self.height//2), int(p1[1] + self.width//2))
+        p2 = (int(p2[0] + self.height//2), int(p2[1] + self.width//2))
+        p3 = (int(p3[0] + self.height//2), int(p3[1] + self.width//2))
 
 
         triangle_contour = np.array([p1, p2, p3])
-        color = tuple(np.random.randint(0, 255, 3))
+        color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
 
         cv2.drawContours(img, [triangle_contour], 0, color, -1)
         return img
 
 
-    def create_hex(self, background=(255, 255, 255)):
+    def create_polygon(self, side=6):
+        def create_object(background=(255, 255, 255)):
+            img = self.create_canvas(background)
+            sizex = np.random.randint(10, self.max_object)
+            sizey = np.random.randint(10, self.max_object)
+            points = [ (int((math.cos(th) + 1) * sizex), int((math.sin(th) + 1) * sizey))
+                for th in [i * (2 * math.pi) / side for i in range(side)]
+                ]  
+    
+            contour = np.array(points)
+            color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
 
-        img = self.create_canvas(background)
-        side = 6
-        sizex = np.random.randint(0, self.max_object)
-        sizey = np.random.randint(0, self.max_object)
-        points = [ ((math.cos(th) + 1) * sizex, (math.sin(th) + 1) * sizey)
-            for th in [i * (2 * math.pi) / side for i in range(side)]
-            ]  
-  
-        contour = np.array(points)
-        color = tuple(np.random.randint(0, 255, 3))
-
-        cv2.drawContours(img, [contour], 0, color, -1)
-        return img
+            cv2.drawContours(img, [contour], 0, color, -1)
+            return img
+        return create_object
 
 
     def create_circle(self, background=(255, 255, 255)):
         img = self.create_canvas(background)
 
-        radius = np.random.randint(0, self.max_object)
-        center = (self.height//2, self.width//2)
-        color = tuple(np.random.randint(0, 255, 3))
+        radius = int(np.random.randint(10, self.max_object))
+        center = (int(self.height//2), int(self.width//2))
+        color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
         cv2.circle(img, center, radius, color, -1)
         return img
 
@@ -105,24 +112,23 @@ class CreateObject(object):
 
         img = self.create_canvas(background)
 
-        img = np.uint8(np.ones((self.height, self.width, 3)))
-        object_height = np.random.randint(0, self.max_object)
-        object_width = np.random.randint(0, self.max_object)
+        object_height = np.random.randint(10, self.max_object)
+        object_width = np.random.randint(5, self.max_object)
 
-        start_point = (self.width//2 - object_width//2, self.height//2 - object_height//2)
-        end_point = (self.width//2 + object_width//2, self.height//2 + object_height//2)
-        color = tuple(np.random.randint(0, 255, 3))
+        start_point = (int(self.width//2 - object_width//2), int(self.height//2 - object_height//2))
+        end_point = (int(self.width//2 + object_width//2), int(self.height//2 + object_height//2))
+        color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
         img = cv2.rectangle(img, 
                 pt1=start_point, 
                 pt2=end_point, 
                 color=color, 
                 thickness=-1)
 
-        radius = object_width//2
-        center = (start_point[0] + object_width//2, start_point[1])
+        radius = int(object_width//2)
+        center = (int(start_point[0] + object_width//2), int(start_point[1]))
         cv2.circle(img, center, radius, color, -1)
 
-        center = (end_point[0] - object_width//2, end_point[1])
+        center = (int(end_point[0] - object_width//2), int(end_point[1]))
         cv2.circle(img, center, radius, color, -1)
         return img
 
@@ -131,21 +137,23 @@ class CreateObject(object):
 
         img = self.create_canvas(background)
 
-        major_axis = np.random.randint(0, self.max_object)
-        minor_axis = np.random.randint(0, self.max_object)
+        major_axis = int(np.random.randint(10, self.max_object))
+        minor_axis = int(np.random.randint(5, self.max_object))
 
-        center = (self.height//2, self.width//2)
+        center = (int(self.height//2), int(self.width//2))
         axesLength = (major_axis, minor_axis)
         
-        angle = np.random.uniform(-np.pi, np.pi)
-        color = tuple(np.random.randint(0, 255, 3))
+        angle = int(np.random.uniform(0, 360))
+        color = tuple([int(a) for a in np.random.randint(0, 255, 3)])
 
         cv2.ellipse(img, 
                     center, 
                     axesLength,
                     angle,
-                    color=color, 
-                    thickness=-1)
+                    0,
+                    360,
+                    color, 
+                    -1)
         return img 
 
 
@@ -153,6 +161,51 @@ class CreateObject(object):
         if not (type in self.objects.keys()): 
             raise ValueError('Unkown type found, allowed types: ', self.objects.keys())
 
-        objects = [self.objects[type] for _ in range(n)]
+        objects = [self.objects[type](background) for _ in range(n)]
         objects = self.transform(images = objects)
         return objects
+
+
+def show_image(images, 
+                title=('original img', 'recon img', 'intervened img'),
+                transpose = False):
+    
+    img_size = 3
+    nrows = len(images)
+    ncols = len(images[0])
+
+    if transpose:
+        ncols = len(images)
+        nrows = len(images[0])
+                
+
+    nimgs = nrows*ncols
+    
+    plt.clf()
+    fig = plt.figure(num = nimgs, figsize=(img_size*ncols, img_size*nrows))
+    fig.tight_layout()
+    gs = gridspec.GridSpec(nrows, ncols)
+    gs.update(wspace=0.2, hspace=0.1)
+
+    for ilist, img_set in enumerate(images):
+
+        for iset, img in enumerate(img_set):
+            xidx = ilist; yidx = iset 
+            if transpose:
+                yidx = ilist; xidx = iset
+
+            ax = plt.subplot(gs[xidx, yidx])
+            ax.imshow(img)
+
+                
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.set_aspect('equal')
+            ax.set_title('$'+title[ilist]+'$')
+            ax.tick_params(bottom='off', top='off', labelbottom='off', right='off', left='off', labelleft='off' )
+
+
+        
+    plt.tight_layout()
+    plt.savefig('test.png')
+    plt.show()
